b713026a5922904951eba540b90d46d260556c3f
Kario Freire <kariofreiretech@gmail.com>
2022-07-29 19:18:12 -0300

    Service - Adiciona validacao temporaria ao service de alunos

--- app/Services/ServiceAlunos.php
+++ app/Services/ServiceAlunos.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Http\Requests\AlunosRequest;
 use App\Http\Requests\StoreAlunosRequest;
 use App\Repositories\Contracts\AlunosRepositoryInterface;
 use Illuminate\Http\Request;
@@ -13,9 +14,9 @@ class ServiceAlunos
 
     /**
      * Define o Repository utilizado neste Service.
-     * 
+     *
      * @param AlunosRepositoryInterface $alunos_repository_interface
-     * 
+     *
      * @return Void
      */
     public function __construct(AlunosRepositoryInterface $alunos_repository_interface)
@@ -25,7 +26,7 @@ class ServiceAlunos
 
     /**
      * Recupera todos os registos em paginação.
-     * 
+     *
      * @return \Illuminate\Contracts\Pagination\Paginator
      */
     public function all()
@@ -35,9 +36,9 @@ class ServiceAlunos
 
     /**
      * Retorna registro por id.
-     * 
+     *
      * @param Int $id
-     * 
+     *
      * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
      */
     public function show(int $id)
@@ -46,53 +47,53 @@ class ServiceAlunos
     }
 
     /**
-     * Deleta um registro.
-     * 
+     * Cria um registro.
+     *
+     * @param Request $request
+     *
+     * @return Bool
+     */
+    public function create(Request $request)
+    {
+        $request->validate([
+            "nome"            => ["required"],
+            "email"           => ["required", "email"],
+            "data_nascimento" => ["required", "date"],
+            "curso_id"        => ["required"]
+        ]);
+
+        return $this->repository->create($request->only(["nome", "email", "data_nascimento", "curso_id"]));
+    }
+
+    /**
+     * Atualiza um registro.
+     *
      * @param Int $id
-     * 
+     * @param Request $dados
+     *
+     * @return Bool
+     */
+    public function update(int $id, Request $request)
+    {
+        $request->validate([
+            "nome"            => ["required"],
+            "email"           => ["required", "email"],
+            "data_nascimento" => ["required", "date"],
+            "curso_id"        => ["required"]
+        ]);
+
+        return $this->repository->update($id, $request->only(["nome", "email", "data_nascimento", "curso_id"]));
+    }
+
+    /**
+     * Deleta um registro.
+     *
+     * @param Int $id
+     *
      * @return Bool
      */
     public function destroy(int $id)
     {
         return $this->repository->destroy($id);
-    }
-
-    /**
-     * Atualiza um registro.
-     * 
-     * @param Int $id
-     * @param Request $dados
-     * 
-     * @return Bool
-     */
-    public function update(int $id, StoreAlunosRequest $request)
-    {
-        $dados = $request->safe()->only([
-            "nome",
-            "email",
-            "data_nascimento",
-            "curso_id"
-        ]);
-
-        return $this->repository->update($id, $dados);
-    }
-
-    /**
-     * Cria um registro.
-     * 
-     * @param Request $request
-     * 
-     * @return Bool
-     */
-    public function create(StoreAlunosRequest $request)
-    {
-        $dados = $request->safe()->only([
-            "nome",
-            "email",
-            "data_nascimento",
-            "curso_id"
-        ]);
-
-        return $this->repository->create($dados);
     }
 }
