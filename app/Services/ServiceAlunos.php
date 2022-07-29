0a62993fb84d937424b4a62ed2ab418a524e4c7b
Kario Freire <kario.freire@fabricainfo.com>
2022-07-29 16:46:19 -0300

    Services - Implementa metodos no service de alunos

--- app/Services/ServiceAlunos.php
+++ app/Services/ServiceAlunos.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\Contracts\AlunosRepositoryInterface;
+use Illuminate\Http\Request;
 
 class ServiceAlunos
 {
@@ -19,5 +20,68 @@ class ServiceAlunos
     public function __construct(AlunosRepositoryInterface $alunos_repository_interface)
     {
         $this->repository = $alunos_repository_interface;
+    }
+
+    /**
+     * Recupera todos os registos em paginação.
+     * 
+     * @return \Illuminate\Contracts\Pagination\Paginator
+     */
+    public function all()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Retorna registro por id.
+     * 
+     * @param Int $id
+     * 
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Collection|static[]|static|null
+     */
+    public function show(int $id)
+    {
+        return $this->repository->show($id);
+    }
+
+    /**
+     * Deleta um registro.
+     * 
+     * @param Int $id
+     * 
+     * @return Bool
+     */
+    public function destroy(int $id)
+    {
+        return $this->repository->destroy($id);
+    }
+
+    /**
+     * Atualiza um registro.
+     * 
+     * @param Int $id
+     * @param Request $dados
+     * 
+     * @return Bool
+     */
+    public function update(int $id, Request $dados)
+    {
+        $dados = [];
+
+        return $this->repository->update($id, $dados);
+    }
+
+    /**
+     * Cria um registro.
+     * 
+     * @param Request $dados
+     * 
+     * @return Bool
+     */
+    public function create(Request $dados)
+    {
+        $dados = [];
+
+        return $this->repository->create($dados);
     }
 }
