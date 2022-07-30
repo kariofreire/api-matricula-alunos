48e451bde292ac6394e7da03394cc20655dff43e
Kario Freire <kariofreiretech@gmail.com>
2022-07-30 13:46:26 -0300

    Seeder - Implementa seed de usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CursosSeeder::class
+            CursosSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
